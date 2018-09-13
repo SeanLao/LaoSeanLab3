719386051e094737258d03634aa8d7ee15a7d680
SeanLao <2015081536@ust-ics.mygbiz.com>
2018-09-13 20:30:20 +0800

    Initial commit

---
+++ app/src/main/java/com/lao/seanmichael/LaoSeanMichaelLab3.java
@@ -0,0 +1,13 @@
+package com.lao.seanmichael;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaoSeanMichaelLab3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lao_sean_michael_lab3);
+    }
+}
